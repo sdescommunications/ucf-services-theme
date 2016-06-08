d5c7fc84d30151588b21e0e72ef7a34d91ff4e7a
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-06-08 17:58:02 -0400

    CPT:StuSvc:hier:scss: add .navbar-gold component and styling

--- single-student_service.php
+++ single-student_service.php
@@ -14,6 +14,28 @@ get_header();
 ?>
 <main class="site-main">
 
+<div style="height: 300px;"></div>
+
+<nav id="student_service-navbar" class="navbar navbar-gold">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<span class="navbar-title">Skip To Section</span>
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#student_service-menu">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+	<div class="collapse navbar-collapse header-center" id="student_service-menu">
+		<ul class="nav navbar-nav">
+			<li><a href="<?= bloginfo( 'url' ); ?>"><?= bloginfo( 'name' ); ?></a></li>
+			<li><a href="<?= the_permalink();; ?>"><?= the_title(); ?></a></li>
+		</ul>
+		<?= WeatherBox::display_weather() ?>
+	</div>
+</nav>
+
 <article>
 
 <?php if (have_posts()) :
