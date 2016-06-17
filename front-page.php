d9bb941dea2286e977508f5472216142db67c0c5
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-06-17 10:35:05 -0400

    front:scss:Search: add search scss, adjust structure of front page

--- front-page.php
+++ front-page.php
@@ -15,6 +15,9 @@ require_once( get_stylesheet_directory() . '/functions/class-sdes-static.php' );
 // WordPress does not allow "<br>" tags within bloginfo('name'), so allow setting width.
 $sitetitle_anchor_width = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-sitetitle_anchor_width', '400px' );
 $sitetitle_anchor_maxwidth = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-sitetitle_anchor_maxwidth', '460px' );
+$frontsearch_lead = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-frontsearch_lead',
+	'From orientation to graduation, the UCF experience creates<br>opportunities that last a lifetime. <b>Let\'s get started</b>.' );
+$frontsearch_placeholder = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-frontsearch_placeholder', 'What can we help you with today?' );
 get_header();
 ?>
 <style>
@@ -44,46 +47,79 @@ get_header();
 
 
 <main>
+  <div class="container">
+	<article class="row page-wrap">
+		<section id="search-frontpage" class="container-fluid">
+			<div class="row search">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="search-lead"><?=  $frontsearch_lead ?></div>
+				</div>
+				<div class="col-md-6 col-md-offset-3 search-bar">
+					<div class="search-bar">
+						<form action="#">
+							<button class="fa fa-search"></button>
+							<input type="search" class="form-control" placeholder="<?= $frontsearch_placeholder ?>">
+						</form>
+					</div>
+				</div>
+			</div>
+		</section> <!-- /.search -->
 
-<article class="col-md-9 col-xs-12">
-	<h2 class="title"><?= the_title() ?></h2>
-	<?php if (have_posts()) :
-		 while (have_posts()) : the_post();
-			the_content();
-		endwhile;
-	else:
-		SDES_Static::Get_No_Posts_Message();
-	endif; ?>
-</article>
+		<div class="container-fluid">
+		  <div class="row">
+			<section id="filter" class="col-md-3 col-md-push-9 side-bar">
+				<span class="filter-by">
+					<h2>Filter By</h2>
+					<div class="panel panel-default">
+						<ul class="list-group">
+							<?php
+							$categories = get_categories( array(
+								'orderby' => 'name',
+								'exclude' => array( 1, ), // Uncategorized.
+								'parent' => 0,
+								'taxonomy' => 'category',
+							) );
+							if ( null !== $categories ) :
+							  foreach ( $categories  as $category ) : ?>
+								<li class="cat-item cat-item-<?= $category->cat_ID ?>">
+									<input class="filter-checkbox" type="checkbox" id="filter-services-<?= $category->cat_ID ?>">
+									<label class="list-group-item filter-label" for="filter-services-<?= $category->cat_ID ?>">
+											<?= $category->name ?>
+									</label>
+								</li>
+							<?php endforeach;
+							else:
+								 echo '<!-- No categories -->';
+							endif; ?>
+						</ul>
+					</div>
+				</span>
+				<div class="clearfix"></div>
 
-<aside class="col-md-2 col-xs-12">
-	<div class="row filter-by">
-		<h2>Filter By</h2>
-		<ul>
-			<?= wp_list_categories( array(
-					'show_option_none' => '<!-- No categories -->',
-					'orderby' => 'title',
-					'exclude' => array( 1, ),
-					'title_li' => '',
-					'depth' => 1,
-					'taxonomy' => 'category',
-				) )
-			?>
-		</ul>
-	</div>
-	<div class="clearfix"></div>
-	<style>
-		.spotlight-wrapper { position: relative; }
-	</style>
-	<div class="row">
-		<div class="spotlight-wrapper">
-		<?php
-			global $post;
-			echo Spotlight::toHTML( get_post_meta( $post->ID, 'page_spotlight', true ) );
-		?>
+				<div class="row">
+					<div class="col-xs-12">
+					<?php
+						global $post;
+						echo Spotlight::toHTML( get_post_meta( $post->ID, 'page_spotlight', true ) );
+					?>
+					</div>
+				</div>
+			</section> <!-- /#filter -->
+
+			<section id="services" class="col-sm-12 col-md-9 col-lg-9 col-md-pull-3">
+				<h2 class="title"><?= the_title() ?></h2>
+				<?php if (have_posts()) :
+					 while (have_posts()) : the_post();
+						the_content();
+					endwhile;
+				else:
+					SDES_Static::Get_No_Posts_Message();
+				endif; ?>
+			</section> <!-- /#services -->
+		  </div>
 		</div>
-	</div>
-</aside>
+	</article>
+  </div>
 </main>
 <div class="clearfix"></div>
 
