1234e88a2f885ced5ab8bf9be916287a3bf2136d
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-07-27 16:53:41 -0400

    hier:NG: move angular dependencies to only front page

--- front-page.php
+++ front-page.php
@@ -18,8 +18,51 @@ $frontsearch_lead = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-f
 	'From orientation to graduation, the UCF experience creates<br>opportunities that last a lifetime. <b>Let\'s get started</b>.' );
 $frontsearch_placeholder = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-frontsearch_placeholder', 'What can we help you with today?' );
 $student_services_api = get_rest_url() . 'rest/v1/services/';
-get_header();
 ?>
+
+<!-- Angular scripts -->
+<!-- Polyfill(s) for older browsers -->
+<script src="https://cdn.jsdelivr.net/core-js/2.4.0/shim.min.js" integrity="sha256-iIdcT94SZY9oCsJj8VTkuvshEfKPXRXaA8nT8lCKG5U=" crossorigin="anonymous"></script>
+
+<script src="https://npmcdn.com/zone.js@0.6.12/dist/zone.js"></script>
+<script src="https://npmcdn.com/reflect-metadata@0.1.3/Reflect.js"></script>
+<script src="https://npmcdn.com/systemjs@0.19.31/dist/system.js"></script>
+<!--
+	<script src="jspm_packages/system.js"></script>
+	<script src="<?= get_stylesheet_directory_uri(); ?>/ng-app/config.js"></script>
+ -->
+<?php
+	function header_load_scripts() {
+		wp_enqueue_script('config-cdn', get_stylesheet_directory_uri() . '/ng-app/config.cdn.js');
+		wp_enqueue_script('config-local', get_stylesheet_directory_uri() . '/ng-app/config.ucf_local.js'); // Set window.ucf_local_config.
+		wp_localize_script('config-cdn', 'configjs', array(
+				'baseURL' => get_stylesheet_directory_uri() . '/ng-app/'
+			));
+		wp_enqueue_script('ng2-bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/ng2-bootstrap/1.0.24/ng2-bootstrap.min.js');
+		wp_add_inline_script('config-local', "
+			System.import('main')
+				  .then(
+				  	function( success ) { },
+				  	function( cdnErr) {
+						// Local fallbacks. See: https://github.com/systemjs/systemjs/issues/986#issuecomment-168422454
+						System.paths = window.ucf_local_config.paths;
+						System.packages = window.ucf_local_config.packages;
+						System.map = window.ucf_local_config.map;
+						System.import('main')
+							  .then(
+							  	function ( success ) { console.info('Successfully loaded from local files after CDN failure: ', cdnErr ); }
+							  , function( err ) {
+							  	console.error( 'Failed loading from CDN: ', cdnErr );
+							  	console.error( err );
+							  } );
+				  });"
+		);
+	}
+	add_action('wp_enqueue_scripts', 'header_load_scripts');
+	
+get_header(); 
+?>
+
 <script>
 <?php
 	$services_limit = SDES_Static::get_theme_mod_defaultIfEmpty( 'services_theme-services_limit', 7 );
