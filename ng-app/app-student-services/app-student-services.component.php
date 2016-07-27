ec76fa172866eb8ec2058c7ec959d53ca74d2688
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-07-27 17:00:10 -0400

    temp:campaign: disable campaigns on front page, in Angular
    Revert after campaign strategy is decided.

--- ng-app/app-student-services/app-student-services.component.php
+++ ng-app/app-student-services/app-student-services.component.php
@@ -46,8 +46,8 @@ $categories = get_categories( array(
                 <div class="col-xs-12">
                     <ucf-campaign [type]="square">
                         <?php
-                            $spotlight_id = get_post_meta( $post->ID, 'page_spotlight', true );
-                            echo do_shortcode( "[campaign spotlight_id='{$spotlight_id}' layout='square']" );
+                            // $spotlight_id = get_post_meta( $post->ID, 'page_spotlight', true );
+                            // echo do_shortcode( "[campaign spotlight_id='{$spotlight_id}' layout='square']" );
                         ?>
                     </ucf-campaign>
                 </div>
@@ -81,8 +81,8 @@ $categories = get_categories( array(
 
             <ucf-campaign [type]="rectangle">
                 <?php
-                    $spotlight_id = get_post_meta( $post->ID, 'page_spotlight', true );
-                    echo do_shortcode( "[campaign spotlight_id='{$spotlight_id}' layout='rectangle']" );
+                    // $spotlight_id = get_post_meta( $post->ID, 'page_spotlight', true );
+                    // echo do_shortcode( "[campaign spotlight_id='{$spotlight_id}' layout='rectangle']" );
                 ?>
             </ucf-campaign>
             <div class="clearfix"></div>
