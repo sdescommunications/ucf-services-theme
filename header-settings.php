51023b4b74fd91590048ad19734a09edfe946ed4
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-11-16 18:16:12 -0500

    Header: load headers tags inside calls to the wp_head action (or a child action)

--- header-settings.php
+++ header-settings.php
@@ -148,6 +148,46 @@ add_action( 'customize_register', __NAMESPACE__.'\Header_Settings::register_head
  */
 class Header {
 	/**
+	 * Load header tags (meta, link, script) to be included on every page.
+	 */
+	public static function header_tags() {
+		ob_start();
+		// @codingStandardsIgnoreStart WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet
+		?>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" >
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+			<link rel="stylesheet" href="<?= get_stylesheet_uri(); ?>" >
+
+			<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js?use-1200-breakpoint=1"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/jquery.validation/1.13.1/jquery.validate.min.js" integrity="sha256-8PU3OtIDEB6pG/gmxafvj3zXSIfwa60suSd6UEUDueI=" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/jquery.validation/1.13.1/additional-methods.min.js" integrity="sha256-TZwF+mdLcrSLlptjyffYpBb8iUAuLtidBmNiMj7ll1k=" crossorigin="anonymous"></script>
+			<script type="text/javascript">
+				(function javascript_fallbacks() {
+					// See: http://stackoverflow.com/a/5531821
+					function document_write_script( src ) {
+						document.write( '<script src="' + src + '">\x3C/script>' );
+					}
+					if ( ! window.jQuery ) { document_write_script( '/js/jquery.min.js' ); }
+					var bootstrap_enabled = ( 'function' === typeof jQuery().modal ); // Will be true if bootstrap is loaded, false otherwise
+					if ( ! bootstrap_enabled ) { document_write_script( '/js/bootstrap.min.js' ); }
+					if ( 'undefined' === typeof jQuery().validate ) { 
+						document_write_script( '/js/jquery.validate.min.js' );
+						document_write_script( '/js/additional-methods.min.js' );
+					}
+				})();
+			</script>
+			<script type="text/javascript" src="<?= get_stylesheet_directory_uri(); ?>/js/sdes_main_ucf.js"></script>
+		<?php
+		// @codingStandardsIgnoreEnd WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet
+		echo ob_get_clean();
+	}
+
+	/**
 	 * Load scripts only needed on the front page (for the Angular app).
 	 * @see wp_enqueue_scripts
 	 */
@@ -276,3 +316,5 @@ class Header {
 		echo ob_get_clean();
 	}
 }
+
+add_action( 'wp_head', __NAMESPACE__.'\Header::header_tags' );
