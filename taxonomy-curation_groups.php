8884231d11f35cc2d082e80f1423159feed6c739
acardona-ucf <adelino.cardona@ucf.edu>
2017-08-15 12:30:27 -0400

    search

--- taxonomy-curation_groups.php
+++ taxonomy-curation_groups.php
@@ -84,15 +84,18 @@ get_header();
 			<?php
 			$args = array(
 				'post_type' => StudentService::NAME,
-				'category' => $cat_id,
+				'category_name' => $cat_name,
 				'orderby' => 'post_title',
 				'order' => 'ASC',
 				'posts_per_page' => -1,
 			);
-			$category_posts = get_posts( $args );
+			$category_posts = new WP_Query( $args );
+
+			die($category_posts);
+
 			if ( 0 !== count( $category_posts ) ) :
 				foreach ( $category_posts as $post ) :	setup_postdata( $post ); ?>
-					<?= StudentService::toHTML( $post ); ?>
+					
 				<?php endforeach;
 			else :
 				SDES_Static::Get_No_Posts_Message();
